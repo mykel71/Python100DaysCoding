52e9498edae9683b92c07f880be9fc50e88d9b70
Michael Shepherd M <shepherd.munemo@outlook.com>
2026-05-04 17:10:13 +0200

    Conditional Statements

--- Day 3/If Else Conditional Statements.py
+++ Day 3/If Else Conditional Statements.py
@@ -44,14 +44,70 @@ elif number < 0:    print(f"{number} is a negative number.")
 
 print("Welcome to the Roller Coaster!")
 height = int(input("What is your height in cm? "))
+bill = 0
+
 if height >= 120:
     print("You can ride the roller coaster!")
     age = int(input("What is your age?"))
     if age <= 12:
-        print("Please pay $5.")
+        bill = 5
+        print("Child Tickets are $5.")
     elif age <= 18:
-        print("Please pay $7.")
+        bill = 15
+        print("Youth Tickets $7.")
     else:
-        print("Please pay $12.")
+        bill = 12
+        print("Adult tickets are $12.")
+
+    wants_photo = input("Would you like to see a photo? (y/n): ")
+    if wants_photo == "y":
+        # add $3 to their bill
+        bill += 3
+
+    print(f"Your final bill is ${bill}")
 else:
     print("Sorry, you are not tall enough to ride the roller coaster.")
+
+
+#Pizza Delivery
+
+print("Welcome to Python Pizza Delivery")
+size = input("What size pizaa do you want? S, M, L: ")
+pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
+extra_chees = input("Do you want some extra cheese? Y or N: ")
+
+## Small Pizza = $15, Medium = #20, Large = $25
+## Add pepperoni to small pizza = $2
+## Add pepperoni to medium and Large = $3
+## Add extra cheese any size  = $1
+
+# todo: work out how much they need to pay based on size and choice.
+bill = 0
+if size == "S":
+    bill += 15
+elif size == "M":
+    bill += 20
+elif size == "L":
+    bill += 25
+else:
+    print("Invalid Inputs")
+
+# todo: work out how much to add to their bill based on their pepperoni choice.
+if pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
+        bill += 3
+# todo: work out their final amount based on whether if they want extra cheese.
+if extra_chees == "Y":
+    bill += 1
+
+print(f"Your final bill is ${bill}")
+
+
+# Logical Operators
+
+# AND, - Both conditions need to be true (True and True)
+# OR, Only one condition needs to be true
+# NOT, the condition must be false
+
--- main.py
+++ main.py
@@ -1,16 +1,1 @@
-weight = 85
-height = 1.85
-
-bmi = weight / (height ** 2)
-
-# 🚨 Do not modify the values above
-# Write your code below 👇
-
-
-
-if bmi < 18.5:
-    print("Your BMI is " + str(round(bmi, 2)) + ", you are underweight.")
-elif bmi < 25:
-    print("Your BMI is " + str(round(bmi, 2)) + ", you have a normal weight.")
-else:
-    print("Your BMI is " + str(round(bmi, 2)) + ", you are slightly overweight.")
+####### Test Area ####
