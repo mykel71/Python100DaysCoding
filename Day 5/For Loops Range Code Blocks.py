56837c379a2a4a16eb139f5added0f0d85d56e83
Michael Shepherd M <shepherd.munemo@outlook.com>
2026-05-12 17:19:12 +0200

    Exercise

--- Day 5/For Loops Range Code Blocks.py
+++ Day 5/For Loops Range Code Blocks.py
@@ -1,6 +1,21 @@
 
 # For Loop
+# loop allows to execute a line of code a multiple times
 
 fruits = ['apple', 'Peach', 'Pear']
 for fruit in fruits:
     print(fruit)
+    print(fruit + ' Pie')
+print(fruits) # this is outside the loops so after the inside code has run this will then run
+
+
+# Mini Exercise
+student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89]
+
+max_score = 0
+
+for score in student_scores:
+    if score > max_score:
+        max_score = score
+
+print(max_score)
