f4e2fe71c876a99e20adb6cf255a42d444b22c29
Michael Shepherd M <shepherd.munemo@outlook.com>
2026-05-12 17:26:47 +0200

    For Each in Range

--- Day 5/For Loops Range Code Blocks.py
+++ Day 5/For Loops Range Code Blocks.py
@@ -19,3 +19,16 @@ for score in student_scores:
         max_score = score
 
 print(max_score)
+
+
+# addition 1 to 100
+# 1 +2 + 3 + 4 + 5 + 6 + 7 +8.......96 + 97 +98 + 99 + 100
+
+# Using For loops with the range function
+
+total = 0
+for number in range(1, 101):
+   total += number
+print(total)
+
+
--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 ####### Test Area ####
 
+# addition 1 to 100
+# 1 +2 + 3 + 4 + 5 + 6 + 7 +8.......96 + 97 +98 + 99 + 100
 
+# Using For loops with the range function
+
+total = 0
+for number in range(1, 101):
+   total += number
+print(total)
